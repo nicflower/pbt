cc030d7065f0574b94a20be4a2e436040f56cfe2
Nicola Fiorella <nicola.fiorella@gmail.com>
2025-10-01 08:56:46 +0200

    initial commmit

---
+++ lib/pbt.ex
@@ -0,0 +1,24 @@
+defmodule Pbt do
+  @moduledoc """
+  Documentation for `Pbt`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Pbt.hello()
+      :world
+
+  """
+  def hello do
+    :ok = foo()
+    :world
+  end
+
+  def foo do
+    :ok
+  end
+
+end
---
+++ test/pbt_test.exs
@@ -0,0 +1,12 @@
+defmodule PbtTest do
+  use ExUnit.Case
+  use PropCheck
+
+  property "always work" do
+    forall type <- term() do
+      boolean(type)
+    end
+  end
+
+  def boolean(_), do: true
+end
